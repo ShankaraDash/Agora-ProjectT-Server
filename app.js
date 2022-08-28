e8cdca4c236995411037ef3bc3b02863ee1adc08
Harsh <harshkaranpuria@gmail.com>
2022-08-29 02:56:26 +0530

    cleanup

--- app.js
+++ app.js
@@ -1,82 +1,94 @@
-var http = require('http');
-const KJUR = require('jsrsasign')
-var express = require('express');
+// Common Imports
+const http = require('http');
+const express = require('express');
 
-var OpenTok = require('opentok');
-var { RtcTokenBuilder, RtcRole } = require('agora-access-token')
+// Agora Imports
+const { RtcTokenBuilder, RtcRole } = require('agora-access-token')
 
+// Twilio Imports
 const AccessToken = require('twilio').jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
 
-// Vonage/TokBox Cred
-var vonageAPIKey = "47546991";
-var vonageSecret = "59203ea5e094de7fff67cb229a9a5d0753d873c5";
+// Vonage Imports
+const OpenTok = require('opentok');
+
+// Zoom Imports
+const KJUR = require('jsrsasign')
+
+const expirationTimeInSeconds = 3600
 
 //Agora Cred 
-var agoraAppID = "5368a8c9785e462197a41827cda88d96";
-var agoraAppCertificate = "e4c907781536450783184d5f00c12f4f";
-var expirationTimeInSeconds = 3600
+let agoraAppID = "5368a8c9785e462197a41827cda88d96";
+let agoraAppCertificate = "e4c907781536450783184d5f00c12f4f";
+
+// Vonage/TokBox Cred
+let vonageAPIKey = "47546991";
+let vonageSecret = "59203ea5e094de7fff67cb229a9a5d0753d873c5";
+
+// Twilio Cred
+let twilioAccountSID = "ACf2dddd4c5aa931567228e5f837ae1c49";
+let twilioSDKSID = "SKeb70b556ae033532f8937be742a80264";
+let twilioSecret = "q6YeGIiM71l2fQcbOsCJgx5Fln981aLk";
 
 //Zoom Cred 
-var ZOOM_API_KEY = "RCSaKX53beXEwKzstNVSdqPk9ERbnMg7kAl6";
+let zoomAPIKey = "RCSaKX53beXEwKzstNVSdqPk9ERbnMg7kAl6";
+let zoomSecret = "RCSaKX53beXEwKzstNVSdqPk9ERbnMg7kAl6";
 
-var app = express();
+let app = express();
 app.disable('x-powered-by');
 app.set('port', 8080);
 app.use(express.favicon());
 app.use(app.router);
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');    
+    // res.setHeader("Access-Control-Allow-Origin", " https://agora-3d464.web.app")
+    // res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
-const VideoGrant = AccessToken.VideoGrant;
-
-function tokenGenerator(identity, room) {
-  // Create an access token which we will sign and return to the client,
-  // containing the grant we just created
-  const token = new AccessToken(
-    'ACf2dddd4c5aa931567228e5f837ae1c49',
-    'SKeb70b556ae033532f8937be742a80264',
-    'q6YeGIiM71l2fQcbOsCJgx5Fln981aLk'
-  );
-
-  // Assign identity to the token
-  token.identity = identity;
-
-  // Grant the access token Twilio Video capabilities
-  const grant = new VideoGrant();
-  grant.room = room;
-  token.addGrant(grant);
-
-  // Serialize the token to a JWT string
-  return token.toJwt();
-}
-
-app.get('/twillio/token', function(request, response) {
-    const identity = request.query.identity || 'identity';
+// Twilio Routes
+app.get('/twillio/token', function (request, response) {
+    const name = request.query.name || 'identity';
     const room = request.query.room;
 
     response.setHeader('Content-Type', 'application/json');
-    response.setHeader("Access-Control-Allow-Origin", "*")
-    response.setHeader("Access-Control-Allow-Origin", " https://agora-3d464.web.app")
-    response.send({"token": tokenGenerator(identity, room)});
+    // response.setHeader("Access-Control-Allow-Origin", "*")
+    // response.setHeader("Access-Control-Allow-Origin", " https://agora-3d464.web.app")
+    response.send({ "token": tokenGenerator(name, room) });
 });
 
+function tokenGenerator(identity, room) {
+    const token = new AccessToken(
+        twilioAccountSID,
+        twilioSDKSID,
+        twilioSecret
+    );
+
+    // Assign identity to the token
+    token.identity = identity;
+
+    // Grant the access token Twilio Video capabilities
+    const grant = new VideoGrant();
+    grant.room = room;
+    token.addGrant(grant);
+
+    // Serialize the token to a JWT string
+    return token.toJwt();
+}
 
 // OpenTok/ Vonage Routes
 var opentok = new OpenTok(vonageAPIKey, vonageSecret);
 var roomToSessionIdDictionary = {};
 
-app.get('/vonage/session', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Origin", " https://agora-3d464.web.app")
-    res.redirect('/vonage/room/session');
-});
+// app.get('/vonage/session', function (req, res) {
+//     res.setHeader('Content-Type', 'application/json');
+//     // res.setHeader("Access-Control-Allow-Origin", "*")
+//     // res.setHeader("Access-Control-Allow-Origin", " https://agora-3d464.web.app")
+//     res.redirect('/vonage/room/session');
+// });
 
 app.get('/vonage/room/:name', function (req, res) {
     var roomName = req.params.name;
@@ -87,7 +99,7 @@ app.get('/vonage/room/:name', function (req, res) {
         // generate token
         token = opentok.generateToken(sessionId);
         res.setHeader('Content-Type', 'application/json');
-        res.header("Access-Control-Allow-Origin", "*")
+        // res.header("Access-Control-Allow-Origin", "*")
 
         res.send({
             apiKey: vonageAPIKey,
@@ -112,7 +124,7 @@ app.get('/vonage/room/:name', function (req, res) {
             // generate token
             token = opentok.generateToken(session.sessionId);
             res.setHeader('Content-Type', 'application/json');
-            res.header("Access-Control-Allow-Origin", "*")
+            // res.header("Access-Control-Allow-Origin", "*")
 
             res.send({
                 apiKey: vonageAPIKey,
@@ -124,7 +136,7 @@ app.get('/vonage/room/:name', function (req, res) {
 });
 
 // Agora Routes
-app.get('/agoraRtcToken', function(req, resp) {
+app.get('/agoraRtcToken', function (req, resp) {
     var role = req.query.role || RtcRole.PUBLISHER;
 
     var currentTimestamp = Math.floor(Date.now() / 1000)
@@ -138,7 +150,7 @@ app.get('/agoraRtcToken', function(req, resp) {
 
     var key = RtcTokenBuilder.buildTokenWithUid(agoraAppID, agoraAppCertificate, channelName, uid, role, privilegeExpiredTs);
 
-    resp.header("Access-Control-Allow-Origin", "*")
+    // resp.header("Access-Control-Allow-Origin", "*")
     return resp.json({ 'key': key }).send();
 });
 
@@ -152,24 +164,24 @@ app.get('/zoomsignature/:name', (req, res) => {
     const oHeader = { alg: 'HS256', typ: 'JWT' }
 
     const oPayload = {
-      app_key: ZOOM_API_KEY,
-      tpc:  req.params.name,
-      session_key: '',
-      iat: iat,
-      exp: exp
+        app_key: zoomAPIKey,
+        tpc: req.params.name,
+        session_key: '',
+        iat: iat,
+        exp: exp
     }
-  
+
     const sHeader = JSON.stringify(oHeader)
     const sPayload = JSON.stringify(oPayload)
-    const signature = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, 'RCSaKX53beXEwKzstNVSdqPk9ERbnMg7kAl6')
-    res.header("Access-Control-Allow-Origin", "*")
+    const signature = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, zoomSecret)
+    // res.header("Access-Control-Allow-Origin", "*")
 
     res.json({
-      signature: signature
+        signature: signature
     })
-  })
+})
 
 
-http.createServer(app).listen(process.env.PORT || app.get('port'), function() {
+http.createServer(app).listen(process.env.PORT || app.get('port'), function () {
     console.log('Agora Server starts at ' + app.get('port'));
 });
